7f8b77b09223acac3b5879cb87baf8fb82613cdc
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2025-02-10 20:28:42 +0600

    misc

--- app/Shortcode.php
+++ app/Shortcode.php
@@ -37,69 +37,72 @@ class Shortcode extends Base {
         return __( 'My Shortcode', 'run-manager' );
     }
 
+  
     function display_race_data_table() {
-    $upload_dir  = wp_upload_dir();
-    $file_path_xlsx = $upload_dir['basedir'] . '/race_data/race_data.xlsx';
-    $file_path_xls  = $upload_dir['basedir'] . '/race_data/race_data.xls';
+        $upload_dir     = wp_upload_dir();
+        $file_path_xlsx = $upload_dir['basedir'] . '/race_data/race_data.xlsx';
+        $file_path_xls  = $upload_dir['basedir'] . '/race_data/race_data.xls';
 
-    // Check if the file exists
-    if (file_exists($file_path_xlsx)) {
-        $file_path = $file_path_xlsx;
-    } elseif (file_exists($file_path_xls)) {
-        $file_path = $file_path_xls;
-    } else {
-        return "<p>No file found.</p>";
-    }
-
-    // Load PHPSpreadsheet to read Excel
-    require_once ABSPATH . 'wp-load.php'; 
-    require_once ABSPATH . 'wp-admin/includes/file.php'; 
-    
-
-    $spreadsheet = IOFactory::load($file_path);
-    $worksheet = $spreadsheet->getActiveSheet();
-    $data = $worksheet->toArray(); 
-
-    // Start building the table
-    $html = '<table id="raceDataTable" class="display"><thead><tr>';
-
-    // Print table headers
-    foreach ($data[0] as $header) {
-        $html .= "<th>{$header}</th>";
-    }
-    $html .= '</tr></thead><tbody>';
-
-    // Print table rows and exclude empty rows or cells
-    foreach (array_slice($data, 1) as $row) {
-        // Check if the row is empty (all cells are empty)
-        if (array_filter($row)) { // If the row contains any non-empty cell
-            $html .= '<tr>';
-            foreach ($row as $cell) {
-                // Only print non-empty cells
-                if (!empty($cell)) {
-                    $html .= "<td>{$cell}</td>";
-                }
-            }
-            $html .= '</tr>';
+        if (file_exists($file_path_xlsx)) {
+            $file_path = $file_path_xlsx;
+        } elseif (file_exists($file_path_xls)) {
+            $file_path = $file_path_xls;
+        } else {
+            return "<p>File not found.</p>";
         }
+
+        $cache_key = 'race_data_file_' . md5($file_path);
+
+        $cached_data = get_transient($cache_key);
+        
+        if ($cached_data === false) {
+            require_once ABSPATH . 'wp-load.php'; 
+            require_once ABSPATH . 'wp-admin/includes/file.php'; 
+            
+
+            $spreadsheet = IOFactory::load($file_path);
+            $worksheet = $spreadsheet->getActiveSheet();
+            $data = $worksheet->toArray();
+
+            set_transient($cache_key, $data, 3600); 
+        } else {
+            $data = $cached_data;
+        }
+
+        $html = '<table id="raceDataTable" class="display"><thead><tr>';
+
+        foreach ($data[0] as $header) {
+            $html .= "<th>{$header}</th>";
+        }
+        $html .= '</tr></thead><tbody>';
+
+        foreach (array_slice($data, 1) as $row) {
+            if (array_filter($row)) {
+                $html .= '<tr>';
+                foreach ($row as $cell) {
+                    if (!empty($cell)) {
+                        $html .= "<td>{$cell}</td>";
+                    }
+                }
+                $html .= '</tr>';
+            }
+        }
+
+        $html .= '</tbody></table>';
+
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('datatables', 'https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js', array('jquery'), null, true);
+        wp_enqueue_style('datatables-css', 'https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css');
+
+        $html .= "<script>
+            jQuery(document).ready(function($) {
+                $('#raceDataTable').DataTable();
+            });
+        </script>";
+
+        return $html;
     }
 
-    $html .= '</tbody></table>';
-
-    // Enqueue DataTables scripts and styles
-    wp_enqueue_script('jquery');
-    wp_enqueue_script('datatables', 'https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js', array('jquery'), null, true);
-    wp_enqueue_style('datatables-css', 'https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css');
-
-    // Add JavaScript to initialize DataTables
-    $html .= "<script>
-        jQuery(document).ready(function($) {
-            $('#raceDataTable').DataTable();
-        });
-    </script>";
-
-    return $html;
-}
 
 }
 
