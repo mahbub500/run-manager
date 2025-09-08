b89aecadc88b44e5551533e3f037b1c33c48fe96
mahbub500 <mahbubmr500@gmail.com>
2025-09-08 08:35:07 +0600

    misc

--- app/Front.php
+++ app/Front.php
@@ -47,7 +47,7 @@ class Front extends Base {
 	public function enqueue_scripts() {
 		$min = defined( 'RUN_MANAGER_DEBUG' ) && RUN_MANAGER_DEBUG ? '' : '.min';
 
-		wp_enqueue_style( $this->slug, plugins_url( "/assets/css/front{$min}.css", RUN_MANAGER ), '',time(), 'all' );
+		wp_enqueue_style( $this->slug, plugins_url( "/assets/css/front{$min}.css", RUN_MANAGER ), '',$this->version, 'all' );
 
 		wp_enqueue_script( $this->slug, plugins_url( "/assets/js/front{$min}.js", RUN_MANAGER ), [ 'jquery' ], $this->version, true );
 		
